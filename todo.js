7ab1bf0c04001b2a3c8b68fe8e3f651390c02adc
Ken Pierce <kpierce8@gmail.com>
2015-08-09 18:09:37 +0000

    add initial file

---
+++ todo.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var url = require("url");
+var items = [];
+
+var server = http.createServer(function(req, res) {
+    switch (req.mode) {
+        case 'POST':
+            var item = '';
+            req.setEncoding('utf8');
+            req.on('data', function(chunk) {
+                item += chunk;
+            });
+            req.on('end', function() {
+                items.push(item);
+                res.end('OK\n');
+            });
+            break;
+        case 'GET':
+            items.forEach(function(item, i) {
+                res.write(i + ') ' + item + '\n');
+            });
+            res.end();
+            break;
+    }
+})
